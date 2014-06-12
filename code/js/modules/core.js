6d5a9a9b7300a50add4647bcf58004053f4d3b72
Tomas Novella <tomasnovella@gmail.com>
2014-06-12 17:52:25 +0200

    started working on unittests...

--- code/js/modules/core.js
+++ code/js/modules/core.js
@@ -27,70 +27,73 @@ function interpretScrapingDirective(directive, context, implicitArgument) {
 }
 
 
-//function fillObject(json) {
-//  var output = {};
-//  _.forOwn(json, function(val, key) {
-//    if (_.isPlainObject(val)) {
-//      output[key] = fillObject(val);
-//    } else {
-//      output[key] = interpretScrapingDirective(val, lllllllll);
-//    }
-//  });
-//  return output;
-//}
 
-function interpretScrapingUnit(scrapingUnit, doneCallback, failCallback) {
+function processActions(actions, context) {
+  _.forEach(actions, function(action){
+    return interpretScrapingDirective(action, context);
+  });
+}
+
+function processTemp(temp, context) {
+  // transform: {name1:tmpVar1, name2:tmpVar2} --> [specialtmpVar1, specialtmpVar2]
+  // where specialTmpVar = {name:..., prio:..., code:...}
+  var transformed = [];
+  _.forEach(temp, function(item, key){
+    var transItem = {name: key};
+    if (_.isPlainObject(item)) { // {prio:..., code:...}
+      transItem.prio = item.prio || 0;
+      transItem.code = item.code;
+    } else { // just an instruction
+      transItem.prio = 0;
+      transItem.code = item;
+    }
+    transformed.push(transItem);
+  });
+
+  _.sortBy(transformed, _.property('prio'));
+
+  _.forEach(transformed, function(item){ // setVal...
+
+    var instr = ['!setVal', interpretScrapingDirective(item.code, context), item.name];
+    interpretScrapingDirective(instr, context);
+
+  });
+}
+
+function processResult(result) {
+  if (_.isPlainObject(result)) {
+    return _.mapValues(result, function(item){
+      return interpretScrapingDirective(item, context);
+    });
+  } else {
+    return interpretScrapingDirective(result, context);
+  }
+}
+function processScrapingUnit(scrapingUnit) {
   var context = { storage: {} };
 
-  function processScrapingUnit() {
-    // 1. process actions
-    var actions = scrapingUnit.actions;
-    if (_.isArray(actions)) {
-      _.forEach(actions, function(action){
-        return interpretScrapingDirective(action, context);
-      });
-    }
-
-    // 2. process temp
-    var temp = scrapingUnit.temp;
-    if (_.isPlainObject(temp)) {
-      // transform: {name1:tmpVar1, name2:tmpVar2} --> [specialtmpVar1, specialtmpVar2]
-      // where specialTmpVar = {name:..., prio:..., code:...}
-      var transformed = [];
-      _.forEach(temp, function(item, key){
-        var transItem = {name: key};
-        if (_.isPlainObject(item)) { // {prio:..., code:...}
-          transItem.prio = item.prio || 0;
-          transItem.code = item.code;
-        } else { // just an instruction
-          transItem.prio = 0;
-          transItem.code = item;
-        }
-        transformed.push(transItem);
-      });
-
-      _.sortBy(transformed, _.property('prio'));
-
-      _.forEach(transformed, function(item){ // setVal...
-        context.storage[item.name] = interpretScrapingDirective(item.code, context);
-      });
-    }
-
-    // 3. process result
-    var result = scrapingUnit.result;
-    if (!result) {
-      throw new exceptions.RuntimeError('No result defined for the scraping unit');
-    }
-
-    if (_.isPlainObject(result)) {
-      return _.mapValues(result, function(item){
-        return interpretScrapingDirective(item, context);
-      });
-    } else {
-      return interpretScrapingDirective(result, context);
-    }
+  // 1. process actions
+  var actions = scrapingUnit.actions;
+  if (_.isArray(actions)) {
+    processActions(actions, context);
   }
 
+  // 2. process temp
+  var temp = scrapingUnit.temp;
+  if (_.isPlainObject(temp)) {
+    processTemp(temp, context);
+  }
+
+  // 3. process result
+  var result = scrapingUnit.result;
+  if (!result) {
+    throw new exceptions.RuntimeError('No result defined for the scraping unit');
+  }
+
+ return processResult(result);
+}
+
+function interpretScrapingUnit(scrapingUnit, doneCallback, failCallback) {
   if (_.isPlainObject(scrapingUnit.waitFor)) {
     var millis = scrapingUnit.waitFor.millis;
     if (_.isUndefined(millis)) {
@@ -119,17 +122,24 @@ function interpretScrapingUnit(scrapingUnit, doneCallback, failCallback) {
     }
     var promise = process();
     promise.done(function() {
-      doneCallback(processScrapingUnit());
+      doneCallback(processScrapingUnit(scrapingUnit));
     }).fail(function(){
         failCallback(scrapingUnit);
     });
 
   } else { // no waitfor, everything simple
-    doneCallback(processScrapingUnit());
+    doneCallback(processScrapingUnit(scrapingUnit));
   }
 }
-
 module.exports = {
+  test: function(dir) {return interpretScrapingDirective(dir, {storage:{} }); },
+
+  // these four functions are exported only for unit testing
+  processActions: processActions,
+  processTemp: processTemp,
+  processResult: processResult,
+  processScrapingUnit: processScrapingUnit,
+
   interpretScrapingDirective: interpretScrapingDirective, // used in commands.js
   interpretScrapingUnit: interpretScrapingUnit
 };
--- code/js/modules/core.spec.js
+++ code/js/modules/core.spec.js
@@ -5,22 +5,17 @@
 var assert = require('assert');
 
 var commands = require('./commands');
-
+var exceptions = require('./exceptions');
 var core = require('./core');
 
 describe('module for testing Serrano core', function() {
-  it('should verify whether the core works well with the storage when executing commands'+
-    ' and everything is put together well', function() {
+  before(function(){
     commands.__setJQuery(require('../libs/jquery-mock'));
+  });
 
-    commands.addCommands({
-      constant: {
-        argumentCount: '1',
-        implicitForeach: false,
-        code: function(c) { return c; }
-      }
-    });
 
+
+  it('should check the `interpretScrapingDirective` function', function() {
     // define instructions
     var context = {storage: {}},
       interpret =  function(directive, implicitArgument) { // shortcut
@@ -36,6 +31,49 @@ describe('module for testing Serrano core', function() {
     assert.strictEqual(interpret(cmd), 'Hello Tomas!');
     assert.strictEqual(interpret(['>!len'],interpret(sel)), 2);
     assert.deepEqual(interpret(instr), '<re>Double filtered paragraph</re>');
+  });
+
+  it('should verify if `actions` from scraping unit is correctly processed', function() {
+  });
+  it('should verify if `temp` from scraping unit is correctly processed', function() {
+    var ctx = {storage:{}};
+
+    var temp = {
+      tmpVar0: ['!constant', 'tmpVal0'],
+      tmpVar1: { // ok
+        prio: 1,
+        code: ['!getVal', 'tmpVar0']
+      }
+    };
+    var context = { storage:{} };
+    core.processTemp(temp, context);
+    assert.strictEqual(context.storage.tmpVar0, 'tmpVal0');
+    assert.strictEqual(context.storage.tmpVar0, 'tmpVal0');
+
+    context = {storage:{}};
+    var tempError = {
+      tmpVar0: ['!constant', 'tmpVal0'],
+      tmpVar1: { // ok
+        prio: 1,
+        code: ['!getVal', 'tmpVar0']
+      },
+      tmpVar2: { // problem, needs value from action of lower importance (higher number)
+        prio: 2,
+        code: ['!getVal', 'tmpVar3']
+      },
+      tmpVar3: {
+        prio: 3,
+        code: ['!constant', 'tmpVal3']
+      }
+    };
+
+    assert.throws(function(){core.processTemp(tempError, context)}, exceptions.RuntimeError);
+  });
+
+  it('should verify if `result` from scraping unit is correctly processed', function() {
+
+  });
+  it('should verify if `waitFor` from scraping unit is correctly processed', function() {
 
   });
 });
