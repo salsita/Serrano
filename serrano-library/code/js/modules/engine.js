18336f11e3cad3d159529bc18aaa8a094a631978
Tomas Novella <tomasnovella@gmail.com>
2014-08-07 17:45:04 +0200

    unittests for engine.exec

--- serrano-library/code/js/modules/engine.js
+++ serrano-library/code/js/modules/engine.js
@@ -5,32 +5,41 @@
 var _ = require('../libs/lodash');
 var interpreter = require('./interpreter');
 var scrapingUnit = require('./scrapingUnit');
+
+
 /**
- * This object contains all the scrapingUnit and actions bound to a given URI.
+ * This object contains all the scrapingUnits and actions bound to a given URI.
  */
 var rules;
 
-
+//todo add comments
 function exec() { // todo context.storage??? ->transfer further...
   var action;
-  var templateContext = {};
+  var templateContext;
 
   if (arguments.length === 0) { // exec()
     action = rules.actions;
+    templateContext = {};
   } else if (arguments.length === 1) { // exec(<templateContext>) and exec(act_name)
-    if (typeof (arguments[0]) === 'string'){ // exec(act_name)
+    if (typeof (arguments[0]) === 'string') { // exec(act_name)
       action = rules.actions[arguments[0]];
+      templateContext = {};
     } else { // exec(<templateContext>)
+      action = rules.actions;
       templateContext = arguments[0];
     }
   } else if(arguments.length === 2) {   // exec(act_name, <templateContext>);
     action = rules.actions[arguments[0]];
-    templateContext = arguments[0];
+    templateContext = arguments[1];
   }
 
   var context = interpreter.createContext();
   context.template = templateContext;
-
+  if (!action) {
+    throw new Error('No valid action selected from the rules object. Arguments '+
+      'array of exec function: '+ JSON.stringify(arguments) +
+      '. Template supplied:'+JSON.stringify(templateContext)+'.');
+  }
   return interpreter.interpretScrapingDirective(action, context);
 }
 
--- serrano-library/code/js/modules/engine.spec.js
+++ serrano-library/code/js/modules/engine.spec.js
@@ -1,3 +1,70 @@
 /**
  * Created by tomasnovella on 8/7/14.
  */
+
+var assert = require('assert');
+
+var commands = require('./commands');
+var engine = require('./engine');
+
+describe('module providing engine functionality', function() {
+  before(function() {
+    commands.addCommands({
+      templateConstant: {
+        argumentCount: '1',
+        code: function(context, arg1) {
+          return [arg1, context.template];
+        }
+      }
+    });
+  });
+
+  after(function() {
+    commands.init();
+  });
+
+  it('should check the engine.exec functionality', function() {
+    var templateContext = { name: 'Fry' };
+
+    engine.setRules({ // single `actions` rules
+      scraping: 'whatever',
+      actions: ['!templateConstant', 'action']
+    });
+
+    // correct input
+    assert.deepEqual(engine.exec(), ["action",{}]);
+    assert.deepEqual(engine.exec(templateContext), ["action", templateContext]);
+
+    // incorrect input
+    assert.throws(function() {engine.exec('nonExistingValue');}, Error);
+    assert.throws(function() {engine.exec('nonExistingValue', templateContext);}, Error);
+
+
+    engine.setRules({ // multiple `actions` rules
+      scraping: 'whatever',
+      actions: {
+        first: ['!templateConstant', 'firstAction'],
+        second: ['!templateConstant', 'secondAction']
+      }
+    });
+
+    // correct input
+    assert.deepEqual(engine.exec('first'), ['firstAction', {}]);
+    assert.deepEqual(engine.exec('second'), ['secondAction', {}]);
+
+    assert.deepEqual(engine.exec('first', templateContext), ['firstAction', templateContext]);
+    assert.deepEqual(engine.exec('second', templateContext), ['secondAction', templateContext]);
+
+
+    // incorrect input
+    assert.throws(function() { engine.exec(); }, Error);
+    assert.throws(function() { engine.exec(templateContext); }, Error);
+
+    assert.throws(function() { engine.exec('notFound'); }, Error);
+    assert.throws(function() { engine.exec('notFound', templateContext); }, Error);
+  });
+
+  it('should check the engine.scrape functionality', function() {
+
+  });
+});
